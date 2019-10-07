5bec078b1b957b2ebce9960a594f6fc7fd697b7b
NUIJIMA Emi <21716070@jwu.ac.jp>
2019-10-07 16:25:11 +0900

    all operators

--- rational.py
+++ rational.py
@@ -7,7 +7,7 @@ class Q(object) :
         if(self.b == 1) :
             return str(self.a)
         else :
-            return f'{self.a} / {self.b}'
+            return f'{self.a}/{self.b}'
 
     def add(self, q) :  #self = q1, q = q2
         a = self.a * q.b + q.a * self.b
@@ -24,6 +24,14 @@ class Q(object) :
         b = self.b * q.b
         return Q(a, b)
 
+    def truediv(self, q) :
+        a = self.a * q.b
+        b = self.b * q.a
+        return Q(a, b)
+
 q1 = Q(2, 5)
 q2 = Q(1, 3)
 print(q1.add(q2))
+print(q1.sub(q2))
+print(q1.mul(q2))
+print(q1.truediv(q2))
