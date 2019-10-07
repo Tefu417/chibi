f29abc326edb1dcbf64de0fe0165ca732a30af32
NUIJIMA Emi <21716070@jwu.ac.jp>
2019-10-07 16:21:32 +0900

    add

--- rational.py
+++ rational.py
@@ -3,11 +3,22 @@ class Q(object) :
         self.a = a
         self.b = b
 
-    # def fraction(self) :
-    #     return(self.a / self.b)
-
     def __repr__(self) :
-        return(str(self.a / self.b))
+        if(self.b == 1) :
+            return str(self.a)
+        else :
+            return f'{self.a} / {self.b}'
 
-q = Q(1, 2)
-print(q)
+    def add(self, q) :  #self = q1, q = q2
+        a = self.a * q.b + q.a * self.b
+        b = self.b * q.b
+        return Q(a, b)
+
+    def sub(self, q) :
+        a = self.a * q.b - q.a * self.b
+        b = self.b * q.b
+        return Q(a, b)
+
+q1 = Q(2, 5)
+q2 = Q(1, 3)
+print(q1.add(q2))
