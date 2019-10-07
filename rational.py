e1a0e76fa2625f0517e44fb88dfbb85e7fb646b4
NUIJIMA Emi <21716070@jwu.ac.jp>
2019-10-07 16:04:20 +0900

    class Q

--- rational.py
+++ rational.py
@@ -1,2 +1,13 @@
 class Q(object) :
-    
+    def __init__(self, a, b) :
+        self.a = a
+        self.b = b
+
+    def fraction(self) :
+        return(self.a / self.b)
+
+    def __repr__(self) :
+        return(str(self))
+
+q = Q(1, 2)
+print(q)
