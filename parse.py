423906117aff2a7c6a3d432daae82c25fc558df9
NUIJIMA Emi <21716070@jwu.ac.jp>
2019-10-28 15:56:09 +0900

    output binary Add

--- parse.py
+++ parse.py
@@ -1,16 +1,22 @@
 from exp import Val, Add
 
 def parse(s : str) :
-    num = int(s)
-    return Val(num)
+    pos = s.find("+")
+    if(pos == 0) :
+        num = int(s)
+        return Val(num)
+    else :
+        s1 = int(s[0 : pos])
+        s2 = int(s[pos+1 :])
+        return Add(Val(s1), Val(s2)) 
 
-e = parse("123")
+e = parse("123+234")
 print(e)
 
-s = "123+234"
-pos = s.find("+")
-print("pos", pos)
+# s = "123+234"
+# pos = s.find("+")
+# print("pos", pos)
 
-s1 = s[0 : pos]
-s2 = s[pos+1 :]
-print(s,s1, s2)
+# s1 = s[0 : pos]
+# s2 = s[pos+1 :]
+# print(s,s1, s2)
