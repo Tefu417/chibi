7bd5ad63af9042de1c86fa4001a8171313df13f2
NUIJIMA Emi <21716070@jwu.ac.jp>
2019-10-28 16:09:09 +0900

    output Add

--- parse.py
+++ parse.py
@@ -1,16 +1,20 @@
 from exp import Val, Add
 
-def parse(s : str) :
+def parse(s: str) :
     pos = s.find("+")
-    if(pos == 0) :
+    if(pos == -1) :
         num = int(s)
         return Val(num)
     else :
-        s1 = int(s[0 : pos])
-        s2 = int(s[pos+1 :])
-        return Add(Val(s1), Val(s2)) 
+        s1 = s[0 : pos]
+        s2 = s[pos+1 :]
+        if(s1.find("+") == -1 and s2.find("+") == -1) :
+            return Add(Val(int(s1)), Val(int(s2)))
+        else :
+            return Add(parse(s1), parse(s2))
 
-e = parse("123+234")
+
+e = parse("1+2+3")
 print(e)
 
 # s = "123+234"
